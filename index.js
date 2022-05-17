0c73cc210993a2b13bde389dab1a30ce133540df
Amira de Moya <demoyaamira@gmail.com>
2022-05-16 21:25:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(location){ 
+    if (location < 42){
+    return 42 - location
+    }
+    
+     return  location - 42 
+    
+}
+
+function distanceFromHqInFeet(location) {
+    let feet = distanceFromHqInBlocks(location)
+    return feet * 264
+   }
+
+
+function distanceTravelledInFeet(startLocation, endLocation){
+    if (startLocation > endLocation) 
+        return (startLocation - endLocation) * 264;
+        
+    else 
+        return (endLocation - startLocation) * 264
+    }
+
+l
+function calculatesFarePrice(startLocation, endLocation) {
+
+if (startLocation > endLocation) 
+return (startLocation - endLocation) * 264;
+
+else 
+return (endLocation - startLocation) * 264
+}
+
