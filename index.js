914c1628673f6a497f8045a95f2133b1d0cbbe3e
Amira de Moya <demoyaamira@gmail.com>
2022-05-16 22:03:10 -0400

    Done.

--- index.js
+++ index.js
@@ -24,12 +24,15 @@ function distanceTravelledInFeet(startLocation, endLocation){
     }
 
 l
-function calculatesFarePrice(startLocation, endLocation) {
-
-if (startLocation > endLocation) 
-return (startLocation - endLocation) * 264;
-
-else 
-return (endLocation - startLocation) * 264
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = (start - destination) * 264
+    if (distanceInFeet <= 400) {
+       return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+        return (distanceInFeet - 400) * .02;
+    }else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return 25;
+    }
+        return "cannot travel that far";
+    
 }
-
